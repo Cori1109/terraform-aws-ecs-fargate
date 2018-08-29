15d55d9c18a4f9ab3d3727a75beee49e373c2744
Cori1109 <chris.jenkins.j1005@gmail.com>
2018-08-29 13:22:18 -0700

    added output

--- out.tf
+++ out.tf
@@ -5,3 +5,7 @@ output "ecs_name_cluster" {
 output "ecs_name_service" {
   value = "${local.ecs_name_service}"
 }
+
+output "ecs_name_task_definition" {
+  value = "${local.ecs_name_task_definition}"
+}
